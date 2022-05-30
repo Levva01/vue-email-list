db3bbdc2e81ef0ecb23f3b913f3569b969855839
Levva01 <vellanifederico13@gmail.com>
2022-05-30 16:30:36 +0200

    Esercizio completato

--- main.js
+++ main.js
@@ -2,8 +2,19 @@ const app = new Vue({
 
     el: '#app',
     data: {
+        mails: [],
 
     },
 
+    created(){
+
+        for(let i = 0; i < 10; i++){
+
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((res) =>{
+                    this.mails.push(res.data.response);
+            });
+
+        }    
+    },
 
 });
